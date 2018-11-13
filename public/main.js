065c919eff42480df373f8b38aab02b70061c01f
delmontz <delmontz@dgl.tokyo>
2018-11-14 02:11:53 +0900

    カウントアプリ

--- public/main.js
+++ public/main.js
@@ -4,11 +4,15 @@ var app = new Vue({
       hoge: 'hello world!!',
       items: ['デルモンテ', 'カゴメ', 'キッコーマン'],
       str: '初期値',
-      show: true
+      show: true,
+      count: 0
    },
    methods: {
       pochitto: function(event){
          alert(event.target);
+      },
+      increment(){ //functionを省くこのも可能　短縮記法
+         this.count++;
       }
    }
 });
